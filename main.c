e139678427906808442835015473767a4176948b
ChenZening <527994785@qq.com>
2018-10-13 15:27:10 +0800

    add the func of line recognition

--- main.c
+++ main.c
@@ -9,11 +9,14 @@
  主要修改之处：将 getc() 改成了自定义的 getnbc() [并没有什么用] 见33、56、57、63行
  将 空格的判断机制函数 getNonBlank() 修改成识别 空格 与 制表符，从而捕获 换行符 见80行
  */
+/*2018-10-13
+ 行数安排上了
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <String.h>
-
+int line=1;
 int charClass;
 #define MAX_LEN 100
 char lexeme[MAX_LEN];
@@ -30,45 +33,42 @@ FILE *inFile;
 enum{WHILE=258,IF,ELSE,FOR,RET,INT,ID,EQU,GEQU,LEQU,ERROR};
 char *keywords[]={"while","if","else","for","return",0};
 
-char getnbc()
-{
+char getnbc()   {
     char ch = fgetc(inFile);
     while (ch == ' ' || ch == '\t') ch = fgetc(inFile);    //略过空格和制表符
     return ch;
 }
 
-void addChar()
-{
-    if(lexLen<=MAX_LEN-2)
-    {
+/*addChar()
+ 存储nextChar
+ */
+void addChar()  {
+    if(lexLen<=MAX_LEN-2)   {
         lexeme[lexLen++]=nextChar;
         lexeme[lexLen]=0;
     }
     else
         printf("ERROR:lexme is too long. \n");
 }
-
-void getChar()
-{
+/*
+ getChar()
+ 第一次运行：读入两个字符
+ 其他：nextChar next2Char后挪一位 判断nextChar的charClass并存储
+ */
+void getChar()  {
     static int firstRun=1;
-    if(firstRun)
-    {
-        nextChar=getnbc(inFile);
-        next2Char=getnbc(inFile);
+    if(firstRun)    {
+        nextChar=getnbc();
+        next2Char=getnbc();
         firstRun=0;
-    }
-    else
-    {
+    }   else    {
         nextChar=next2Char;
-        next2Char=getnbc(inFile);
+        next2Char=getnbc();
     }
     
-    if(nextChar==EOF)
-    {
+    if(nextChar==EOF)   {
         charClass=EOF;
-    }
-    else
-    {
+    }   else    {
         if(isalpha(nextChar))
             charClass=LETTER;
         else if(isdigit(nextChar))
@@ -77,17 +77,14 @@ void getChar()
             charClass=UNKNOWN;
     }
 }
-void getNonBlank()
-{
+void getNonBlank()  {
     while (nextChar==' '|| nextChar=='\t') {
         getChar();
     }
 }
 
-int checkSymbol(char ch, char nextCh)
-{
-    switch(ch)
-    {
+int checkSymbol(char ch, char nextCh)   {
+    switch(ch)  {
         case'(':case')':case';':case'+':case'-':
             addChar();
             nextToken=ch;
@@ -95,8 +92,7 @@ int checkSymbol(char ch, char nextCh)
         case'=':
             addChar();
             nextToken=ch;
-            if(nextCh == '=')
-            {
+            if(nextCh == '=')   {
                 getChar();
                 addChar();
                 nextToken=EQU;
@@ -105,8 +101,7 @@ int checkSymbol(char ch, char nextCh)
         case'>':
             addChar();
             nextToken=ch;
-            if(nextCh == '=')
-            {
+            if(nextCh == '=')   {
                 getChar();
                 addChar();
                 nextToken=GEQU;
@@ -115,12 +110,16 @@ int checkSymbol(char ch, char nextCh)
         case'<':
             addChar();
             nextToken=ch;
-            if(nextCh == '=')
-            {
+            if(nextCh == '=')   {
                 getChar();
                 addChar();
                 nextToken=LEQU;
             }
+            break;
+        case '\n':
+            addChar();
+            line++;
+            nextToken=27;
             break;
         case EOF:
             addChar();
@@ -132,14 +131,11 @@ int checkSymbol(char ch, char nextCh)
     return nextToken;
 }
 
-void checkKeywords(char* pword)
-{
+void checkKeywords(char* pword) {
     int i = 0;
-    while(keywords[i] != 0)
-    {
+    while(keywords[i] != 0) {
         char* pkeyword = keywords[i];
-        if(strcmp(pword,pkeyword) == 0)
-        {
+        if(strcmp(pword,pkeyword) == 0) {
             nextToken = 258 + i;
             return;
         }
@@ -147,17 +143,14 @@ void checkKeywords(char* pword)
     }
 }
 
-int lexer()
-{
+int lexer() {
     lexLen = 0;
     getNonBlank();
-    switch(charClass)
-    {
+    switch(charClass)   {
         case LETTER:
             addChar();
             getChar();
-            while(charClass == LETTER || charClass == DIGIT)
-            {
+            while(charClass == LETTER || charClass == DIGIT)    {
                 addChar();
                 getChar();
             }
@@ -169,8 +162,7 @@ int lexer()
         case DIGIT:
             addChar();
             getChar();
-            while(charClass == DIGIT)
-            {
+            while(charClass == DIGIT)   {
                 addChar();
                 getChar();
             }
@@ -191,27 +183,24 @@ int lexer()
             break;
     }
     
-    printf(" <%6d,  %s  >\n",nextToken,lexeme);
+    printf("line %02d:(%03d,numbers) (class):%s\n",line,nextToken,lexeme);
     return nextToken;
 }
 
 
-void main(int argc, const char * argv[]) {
-    if (argc<2)
-    {
+void main(int argc,const char * argv[]) {
+    if (argc<2) {
         printf("ERROR:input file name is needed.\n");
         exit(0);
     }
     inFile=fopen(argv[1], "r");
-    if (inFile==NULL)
-    {
+    if (inFile==NULL)   {
         printf("ERROR:can not open file.\n");
         exit(0);
     }
     
     getChar();
-    while (nextToken!=EOF)
-    {
+    while (nextToken!=EOF)  {
         lexer();
     }
     
