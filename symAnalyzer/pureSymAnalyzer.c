26bafcf78feb1c0b9ea738b4f3905162ac35e5e8
Wenjie Huang <knsugit@outlook.com>
2018-11-21 14:48:24 +0800

    copy chen's code

--- symAnalyzer/pureSymAnalyzer.c
+++ symAnalyzer/pureSymAnalyzer.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-
+#define MAXQUEUE 200
 void E();
 void E1();
 void T();
@@ -10,10 +10,14 @@ void T1();
 void F();
 void error();
 void advance();
-
+char Queue[MAXQUEUE];
 char sym;
 FILE* inFile;
-
+int head=0,end=0;
+void InitQueue();
+void AddQueue();
+void OutQueue();
+void OutPutCur();
 int main(int argc,const char * argv[])  {
     inFile=fopen(argv[1],"r");
 
@@ -21,27 +25,46 @@ int main(int argc,const char * argv[])  {
         printf("ERROR:can not open file.\n");
         exit(0);
     }
-    advance();
-    
-    while (sym!=EOF){
-        E();
-    }
+    InitQueue();
+    OutQueue();
+    E();
     
 }
-
+void OutPutCur()    {
+    printf("已分析过的串：");
+    for(int i=0;i<head-1;i++)
+        printf("%c ",Queue[i]);
+    printf("\n");
+    printf("当前字符：%c\n",sym);
+    printf("剩余串：");
+    for(int i=head;i<end;i++)
+        printf("%c ",Queue[i]);
+    printf("\n");
+}
 void advance() {
     sym = getc(inFile);
     if (sym == ' ' || sym == '\t'){
         while(sym == ' ' || sym == '\t')
             sym = getc(inFile);
     }
-    printf("\n!!!%c!!!\n", sym);
 }
-
+void InitQueue()    {
+    advance();
+    while (sym!=EOF) {
+        AddQueue();
+        advance();
+    }
+}
+void AddQueue() {
+    Queue[end++]=sym;
+}
+void OutQueue()  {
+    sym=Queue[head++];
+}
 void E() {
     printf("=================================================\n");
     printf("使用的产生式：E --> TE\'\n");
-    //outputCur(p);
+    OutPutCur();
     printf("=================================================\n");
     T();
     E1();
@@ -51,25 +74,25 @@ void E1() {
     if(sym == '+') {
         printf("=================================================\n");
         printf("使用的产生式：E\' --> +TE\'\n");
-        //outputCur(p);
+        OutPutCur();
         printf("=================================================\n");
-        advance();
+        OutQueue();
         T();
         E1();
     }
     else if(sym == '-') {
         printf("=================================================\n");
         printf("使用的产生式：E\' --> -TE\'\n");
-        //outputCur(p);
+        OutPutCur();
         printf("=================================================\n");
-        advance();
+        OutQueue();
         T();
         E1();
     }
     else{
         printf("=================================================\n");
         printf("使用的产生式：E\' --> ε\n");
-        //outputCur(p);
+        OutPutCur();
         printf("=================================================\n");
     }
 }
@@ -77,7 +100,7 @@ void E1() {
 void T() {
     printf("=================================================\n");
     printf("使用的产生式：T --> FT\'\n");
-    //outputCur(p);
+    OutPutCur();
     printf("=================================================\n");
     F();
     T1();
@@ -87,25 +110,25 @@ void T1() {
     if(sym == '*') {
         printf("=================================================\n");
         printf("使用的产生式：T\' --> *FT\'\n");
-        //outputCur(p);
+        OutPutCur();
         printf("=================================================\n");
-        advance();
+        OutQueue();
         F();
         T1();
     }
     else if(sym == '/') {
         printf("=================================================\n");
         printf("使用的产生式：T\' --> *FT\'\n");
-        //outputCur(p);
+        OutPutCur();
         printf("=================================================\n");
-        advance();
+        OutQueue();
         F();
         T1();
     }
     else{
         printf("=================================================\n");
         printf("使用的产生式：T\' --> ε\n");
-        //outputCur(p);
+        OutPutCur();
         printf("=================================================\n");
     }
 }
@@ -114,29 +137,59 @@ void F() {
     if(sym == '(') {
         printf("=================================================\n");
         printf("使用的产生式：F --> (E)\n");
-        //outputCur(p);
+        OutPutCur();
         printf("=================================================\n");
-        advance();
+        OutQueue();
         E();
         if(sym == ')') {
-            advance();
+            OutQueue();
         }
         else {
             error();
         }
     }
-    else if(sym == 'i') {
+    else if(isdigit(sym)) {
         printf("=================================================\n");
         printf("使用的产生式：F --> i\n");
-        //outputCur(p);
+        printf("已分析过的串：");
+        for(int i=0;i<head-1;i++)
+            printf("%c ",Queue[i]);
+        printf("\n");
+        printf("当前字符：");
+        while (isdigit(sym)) {
+            printf("%c ",sym);
+            OutQueue();
+        }
+        printf("\n");
+        printf("剩余串：");
+        for(int i=head;i<end;i++)
+            printf("%c ",Queue[i]);
+        printf("\n");
         printf("=================================================\n");
-        advance();
+    }
+    else if(isalpha(sym))   {
+        printf("=================================================\n");
+        printf("使用的产生式：F --> i\n");
+        printf("已分析过的串：");
+        for(int i=0;i<head-1;i++)
+            printf("%c ",Queue[i]);
+        printf("\n");
+        printf("当前字符：");
+        while (isalpha(sym)) {
+            printf("%c ",sym);
+            OutQueue();
+        }
+        printf("\n");
+        printf("剩余串：");
+        for(int i=head;i<end;i++)
+            printf("%c ",Queue[i]);
+        printf("\n");
+        printf("=================================================\n");
     }
     else{
         error();
     }
 }
-
 void error(){
     printf("ERROR\n");
     exit(-1);
